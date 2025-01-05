ee3e30bfe1ddf38068d6cdb3e2f3c1b38fc5ce6e
Always-Learningeek <myrealemail1400@gmail.com>
2025-01-05 23:30:53 +0100

    A series of changes were made to the view template and URLs in the blog app.
    added blog app URLs , updated views, changed base , blog-single, blog-home

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('home', blog_view, name='blog-home'),
-    path('single', blog_single, name='blog-single'),
+    path('<int:pid>', blog_single, name='blog-single'),
     path('test', test, name='test'),
 
 ]
