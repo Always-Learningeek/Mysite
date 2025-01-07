a9b85947fc1d3888effcb4714c92d28621dc310a
Always-Learningeek <myrealemail1400@gmail.com>
2025-01-07 20:25:56 +0100

    updated

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from blog.models import *
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date')
+    list_display = ('title', 'author', 'counted_views', 'status', 'published_date', 'created_date')
     list_filter = ('status',)
     date_hierarchy = 'published_date'
     search_fields = ['title','content']
