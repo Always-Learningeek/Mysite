078e52dd63194443ac206a7018e041b70b6aa572
Always-Learningeek <myrealemail1400@gmail.com>
2025-01-07 22:23:56 +0100

    added field image

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post (models.Model):
-    author=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
-    #image = models.ImageField(upload_to = 'images/')
+    author=models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
+    image = models.ImageField(upload_to = 'blog/', default='blog/default.jpg')
     title = models.CharField(max_length=255)
     content = models.TextField()
     #category = models.CharField(max_length=255)
