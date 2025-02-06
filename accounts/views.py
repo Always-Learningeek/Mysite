38c45aff8be4a06ef3a8bcdea4f5218555c818a8
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-06 15:13:36 +0100

    New app (accounts) added
    login, logout, signup/register added to my project

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout_view(request):
+    pass
+
+
+def signup_view(request):
+    return render(request, 'accounts/signup.html')
