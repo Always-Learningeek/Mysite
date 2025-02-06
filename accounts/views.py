bc29583142368ca64c757633b8f77a9de2163fef
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-06 16:21:27 +0100

    updated

--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 
 
 def login_view(request):
-    if request.user.is_authenticated:
-        msg = f'You are already logged in {request.user.username}!'
-    else:
-        msg = 'You are not logged in!'
-
-    context = {'msg': msg}
-    return render(request, 'accounts/login.html', context)
+    return render(request, 'accounts/login.html')
 
 
 def logout_view(request):
