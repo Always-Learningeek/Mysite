cf11a4b0b07da8f56fa7efe2520d6237ed87b546
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-09 00:58:38 +0100

    fixed bugs

--- accounts/views.py
+++ accounts/views.py
@@ -4,12 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_protect
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib import messages
-from django.views.decorators.csrf import csrf_protect
+from .forms import SignUpForm
 
 
 @csrf_protect
@@ -63,74 +58,10 @@ def signup_view(request):
                 user = form.save()
                 login(request, user)
                 return redirect('/')
-
         else:
-            #context = {'form': form}
-            #return render(request, 'accounts/signup.html', context)
+
             return redirect('/')
     else:
         form = UserCreationForm()
         context = {'form': form}
         return render(request, 'accounts/signup.html', context)
-
-
-@csrf_protect
-def login_view(request):
-    if not request.user.is_authenticated:
-        if request.method == 'POST':
-            form = AuthenticationForm(request=request, data=request.POST)
-            if form.is_valid():
-                username_or_email = form.cleaned_data.get('username', 'email')
-                password = form.cleaned_data.get('password')
-                if '@' in username_or_email:
-                    try:
-                        user = authenticate(request, username=username_or_email, password=password)
-                        messages.success(request, 'Login via email successful!.')
-                    except User.DoesNotExist:
-                        user = None
-                else:
-                    user = authenticate(request, username=username_or_email, password=password)
-                    messages.success(request, 'Logged in successfully.')
-
-                if user is not None:
-                    login(request, user)
-                    return redirect('/')
-
-            else:
-                context = {'form': form, 'error': 'Invalid credentials or form'}
-                return render(request, 'accounts/login.html', context)
-        else:
-            form = AuthenticationForm()
-            context = {'form': form}
-            return render(request, 'accounts/login.html', context)
-    else:
-        return redirect('/')
-
-
-@login_required
-def logout_view(request):
-    logout(request)
-    return redirect('/')
-
-
-def signup_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/')
-        else:
-            #context = {'form': form}
-            #return render(request, 'accounts/signup.html', context)
-            return redirect('/')
-    else:
-        form = UserCreationForm()
-        context = {'form': form}
-        return render(request, 'accounts/signup.html', context)
-
-
-#def forgot_password_view(request):
-    #x = auth_views.PasswordResetView.as_view()
-    #return redirect('accounts/signup.html')
-    #return render(request, 'accounts/password_reset_form.html',x)
