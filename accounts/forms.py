8af347bb632397fe73279704801b0d1091191dc5
Always-Learningeek <myrealemail1400@gmail.com>
2025-02-10 21:46:26 +0100

    fixed bugs in sign-up and sign-in
    Two custom forms were created in the account section. The view in the user account section has also been updated and revised. The requirements.txt has been updated, and the login.html has also been updated.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,10 @@
 from django.forms import ModelForm
 from captcha.fields import CaptchaField
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+#from django.contrib.auth.forms import AuthenticationForm
+
 
 class LoginForm(ModelForm):
     captcha = CaptchaField()
@@ -8,3 +12,22 @@ class LoginForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class CustomAuthenticationForm(forms.Form):
+    username_or_email = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,33 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+#from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.models import User
+from accounts.forms import  CustomUserCreationForm, CustomAuthenticationForm
 
-
+'''
 @csrf_protect
 def login_view(request):
     if not request.user.is_authenticated:
         if request.method == 'POST':
             form = AuthenticationForm(request=request, data=request.POST)
             if form.is_valid():
-                username_or_email = form.cleaned_data.get('username', 'email')
+                username_or_email = form.cleaned_data.get('username')
                 password = form.cleaned_data.get('password')
+                user = None
                 if '@' in username_or_email:
                     try:
-                        user = authenticate(request, username=username_or_email, password=password)
+                        user_with_email = User.objects.get(email=username_or_email)
+                        user = authenticate(request, username=user_with_email.username, password=password)
                         messages.success(request, 'Login via email successful!.')
                     except User.DoesNotExist:
                         user = None
-                else:
+                if user is None:
                     user = authenticate(request, username=username_or_email, password=password)
-                    messages.success(request, 'Logged in successfully.')
-
+                    if user:
+                        messages.success(request, 'Logged in successfully.')
                 if user is not None:
                     login(request, user)
                     return redirect('/')
-
+                else:
+                    messages.error(request, 'Invalid credentials or form.')
             else:
                 context = {'form': form, 'error': 'Invalid credentials or form'}
                 return render(request, 'accounts/login.html', context)
@@ -37,6 +42,40 @@ def login_view(request):
             return render(request, 'accounts/login.html', context)
     else:
         return redirect('/')
+'''
+
+@csrf_protect
+def login_view(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = CustomAuthenticationForm(request.POST)
+            if form.is_valid():
+                username_or_email = form.cleaned_data.get('username_or_email')
+                password = form.cleaned_data.get('password')
+                try:
+                    if '@' in username_or_email:
+                        user = User.objects.get(email=username_or_email)
+                    else:
+                        user = User.objects.get(username=username_or_email)
+                    user = authenticate(request, username=user.username, password=password)
+                    if user is not None:
+                        login(request, user)
+                        messages.success(request, 'Login successful!')
+                        return redirect('/')
+                    else:
+                        messages.error(request, 'Invalid credentials.')
+                except User.DoesNotExist:
+                    messages.error(request, 'User with this email or username does not exist.')
+            else:
+                messages.error(request, 'Invalid form.')
+        else:
+            form = CustomAuthenticationForm()
+
+        context = {'form': form}
+        return render(request, 'accounts/login.html', context)
+    else:
+        return redirect('/')
+
 
 
 @login_required
@@ -44,13 +83,16 @@ def logout_view(request):
     logout(request)
     return redirect('/')
 
-
+'''
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            if '@' in form:
-                user = form.save()
+            email = form.cleaned_data.get('email')
+            if email and '@' in email:
+                user = form.save(commit=False)
+                user.email = email
+                user.save()
                 login(request, user)
                 return redirect('/')
             else:
@@ -58,9 +100,23 @@ def signup_view(request):
                 login(request, user)
                 return redirect('/')
         else:
-
             return redirect('/')
     else:
         form = UserCreationForm()
+        context = {'form': form}
+        return render(request, 'accounts/signup.html', context)
+'''
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/')
+    else:
+        form = CustomUserCreationForm()
         context = {'form': form}
         return render(request, 'accounts/signup.html', context)
